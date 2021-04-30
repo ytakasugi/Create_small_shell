7a8e20718f0a104a459cfa06bf854c03b4907a80
ytakasugi <sh7.tibi0129@gmail.com>
2021-05-01 00:06:46 +0900

    change file

--- create_small_shell/target/debug/create_small_shell.d
+++
@@ -1,1 +0,0 @@
-/home/ytakasugi/001_CreateSmallShell/create_small_shell/target/debug/create_small_shell: /home/ytakasugi/001_CreateSmallShell/create_small_shell/src/main.rs
--- create_small_shell/target/debug/deps/create_small_shell-7f6732a58bb72093.d
+++
@@ -1,5 +0,0 @@
-/home/ytakasugi/001_CreateSmallShell/create_small_shell/target/debug/deps/create_small_shell-7f6732a58bb72093: src/main.rs
-
-/home/ytakasugi/001_CreateSmallShell/create_small_shell/target/debug/deps/create_small_shell-7f6732a58bb72093.d: src/main.rs
-
-src/main.rs:
