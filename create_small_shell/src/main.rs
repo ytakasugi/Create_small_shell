a6cd21034c7d6bfc86e2dc187c1edc8c4c9d79f6
ytakasugi <sh7.tibi0129@gmail.com>
2021-04-30 23:50:32 +0900

    change file

--- create_small_shell/src/main.rs
+++ create_small_shell/src/main.rs
@@ -1,3 +1,16 @@
+//use std::env;
+use std::io::{stdin/*, stdout, Write*/};
+//use std::path::Path;
+use std::process::{/*Child,*/ Command/*, Stdio*/};
+
 fn main() {
-    println!("Hello, world!");
+    // 新しい空のStringを作成
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+
+    // `read_line`は末尾に改行を残しますが、`trim`はこれを削除します。
+    let command = input.trim();
+
+    // 
+    Command::new(command).spawn().unwrap();
 }
---
+++ create_small_shell/target/debug/create_small_shell.d
@@ -0,0 +1,1 @@
+/home/ytakasugi/001_CreateSmallShell/create_small_shell/target/debug/create_small_shell: /home/ytakasugi/001_CreateSmallShell/create_small_shell/src/main.rs
