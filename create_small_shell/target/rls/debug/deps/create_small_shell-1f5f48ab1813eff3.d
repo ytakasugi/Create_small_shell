e0d55fbb483eeddd3eb1a0d4f70f2caf758604f4
ytakasugi <sh7.tibi0129@gmail.com>
2021-07-11 21:08:09 +0900

    change file

--- create_small_shell/target/rls/debug/deps/create_small_shell-1f5f48ab1813eff3.d
+++
@@ -1,5 +0,0 @@
-/home/ytakasugi/001_CreateSmallShell/create_small_shell/target/rls/debug/deps/create_small_shell-1f5f48ab1813eff3.rmeta: src/main.rs
-
-/home/ytakasugi/001_CreateSmallShell/create_small_shell/target/rls/debug/deps/create_small_shell-1f5f48ab1813eff3.d: src/main.rs
-
-src/main.rs:
--- create_small_shell/target/rls/debug/deps/create_small_shell-24d1ab5c5e6b7bba.d
+++
@@ -1,5 +0,0 @@
-/home/ytakasugi/001_CreateSmallShell/create_small_shell/target/rls/debug/deps/create_small_shell-24d1ab5c5e6b7bba.rmeta: src/main.rs
-
-/home/ytakasugi/001_CreateSmallShell/create_small_shell/target/rls/debug/deps/create_small_shell-24d1ab5c5e6b7bba.d: src/main.rs
-
-src/main.rs:
